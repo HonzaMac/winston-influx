2ab525f266815a1fc63d1d660f16f8b5312144c6
Jan Machala <jan.machala@merck.com>
2021-03-07 00:25:58 +0100

    Enable eslint for TS

--- __tests__/Batcher.test.ts
+++ __tests__/Batcher.test.ts
@@ -1,45 +1,45 @@
-import Batcher from "../src/Batcher"
+import Batcher from '../src/Batcher'
 
 
-jest.useFakeTimers();
+jest.useFakeTimers()
 
 
 describe('batcher', () => {
 
     it('sends a batch when the max size is reached', () => {
-        expect.assertions(2);
-        let batcher = new Batcher({
+        expect.assertions(2)
+        const batcher = new Batcher({
             batchSize: 5
-        });
-        let flushed = false;
+        })
+        let flushed = false
         batcher.on('flush', function () {
-            flushed = true;
-        });
+            flushed = true
+        })
 
         for (let i = 0; i < 4; i++) {
-            batcher.write(i);
+            batcher.write({message: `${i}`, level: 'info'})
         }
-        expect(flushed).toBe(false);
-        batcher.write(1);
-        expect(flushed).toBe(true);
-    });
+        expect(flushed).toBe(false)
+        batcher.write({message: '1', level: 'info'})
+        expect(flushed).toBe(true)
+    })
 
     it('sends a batch when the interval has passed', async () => {
-        expect.assertions(2);
-        let batcher = new Batcher({
+        expect.assertions(2)
+        const batcher = new Batcher({
             interval: 1
-        });
+        })
 
-        let flushed = false;
+        let flushed = false
         batcher.on('flush', function () {
-            flushed = true;
-        });
-        batcher.write(0);
-        expect(flushed).toBe(false);
+            flushed = true
+        })
+        batcher.write({message: '0', level: 'info'})
+        expect(flushed).toBe(false)
 
-        jest.advanceTimersByTime(1000);
+        jest.advanceTimersByTime(1000)
 
-        expect(flushed).toBe(true);
-        batcher.close();
-    });
-});
+        expect(flushed).toBe(true)
+        batcher.close()
+    })
+})
